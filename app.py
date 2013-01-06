9fd00bbde658c289769b794d36664a88e72f192e
Hugo Ruscitti <hugoruscitti@gmail.com>
2013-01-06 00:54:01 -0300

    Creando modelo de datos.

--- app.py
+++ app.py
@@ -5,9 +5,5 @@ app = Flask(__name__)
 app.config.from_object('config.Configuration')
 db = Database(app)
 
-import models
-
-models.create_tables(db)
-
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
---
+++ deploy.py
@@ -0,0 +1,2 @@
+import main
+main.create_tables()
